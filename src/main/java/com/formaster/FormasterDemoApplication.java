79deec476393635f26df99b3582874442f564095
Mayank <Kanakiyamayank182003@gmail.com>
2025-02-12 14:49:21 +0530

    12-02-2025
    Create project

---
+++ src/main/java/com/formaster/FormasterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.formaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormasterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormasterDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formaster/FormasterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormasterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
